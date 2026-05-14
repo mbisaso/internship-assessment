5af5ef2c7b21c7b308f2ac87408ba4ca6824eaa8
mbisaso <marvin.bisaso99@gmail.com>
2026-05-14 16:11:42 +0300

    Added exercises

--- exercises/basics.py
+++ exercises/basics.py
@@ -12,6 +12,14 @@ def collatz(n: int) -> List[int]:
     For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
     So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
     """
+    results = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        results.append(n)
+    return results
     pass
 
 
@@ -21,4 +29,8 @@ def distinct_numbers(numbers: List[int]) -> int:
 
     E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
     """
-    pass
+    return len(set(numbers))    #set() automatically removes duplicates, then len()counts whats left
+    
+    
+
+
